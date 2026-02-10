597a4c62deb9b3a3c2361f5c32868778d563937f
Jamesp3301 <a25075@esenviseu.net>
2026-02-10 09:49:38 +0000

    Classes
    did small classes and start of form

---
+++ MovieManager/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManager
+{
+    internal class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+    public Movie(int id, string title, string director, int year, string genre) 
+    {
+            Id = id;
+            Title = title;
+            Director = director;
+            Year = year;
+            Genre = genre;
+    }
+
+
+}
